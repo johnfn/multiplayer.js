6080df46137e2c6e271020fdf1bd10d858b2e33e
johnfn <johnfn@gmail.com>
2014-11-25 19:37:43 -0800

    Initial commit

---
+++ src/server-core.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var util = require('util');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('views', __dirname + '/../views');
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', function (req, res) {
+  console.log('!');
+
+  res.render('index');
+});
+
+var server = http.listen(8000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Running game at http://%s:%s', host, port)
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+
+  io.emit('map-generation', {
+    for: 'everyone'
+  });
+
+  socket.on('message', function(msg) {
+    console.log("message received:", msg);
+  });
+});
